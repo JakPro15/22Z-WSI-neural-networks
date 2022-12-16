7f588060dec610ca0738cbf52c26e82b8d6f91a2
Paweł Kochański <pawilioxion@gmail.com>
2022-12-16 15:20:35 +0100

    wrote backwards propagation

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -5,7 +5,7 @@ import numpy as np
 class MultilayerPerceptron:
     def __init__(
         self, weights: Sequence[np.ndarray], biases: Sequence[np.ndarray],
-        activation: Callable[[float], float]
+        activation: tuple[Callable[[float], float]]
     ) -> None:
         """
         Creates a multilayer perceprtion with the given weights and the
@@ -17,7 +17,8 @@ class MultilayerPerceptron:
         """
         self.weights = weights
         self.biases = biases
-        self.activation = np.vectorize(activation)
+        self.activation = np.vectorize(activation[0])
+        self.activation_derivative = np.vectorize(activation[1])
 
     def predict(self, attributes: np.ndarray) -> np.ndarray:
         """
@@ -29,6 +30,51 @@ class MultilayerPerceptron:
             data = np.matmul(weights, data) + biases
             if layer < len(self.weights) - 1:
                 data = self.activation(data)
+        return data
+
+    def propagate(self, attributes: np.ndarray,
+                  learning_rate: float, target: np.ndarray) -> np.ndarray:
+        """
+        Predicts the target vector for the given attributes vector.
+        """
+        data = attributes
+        sums = [data]
+        inputs = [data]
+        for layer, (weights, biases) in \
+                enumerate(zip(self.weights, self.biases)):
+            data = np.matmul(weights, data) + biases
+            sums.append(data)
+            if layer < len(self.weights) - 1:
+                data = self.activation(data)
+            inputs.append(data)
+
+        for i in reversed(range(len(self.weights))):
+            if np.array_equal(self.weights[i], self.weights[-1]):
+                deltas = []
+                for j in range(len(self.weights[i])):
+                    delta = (inputs[-1][j] - target[j]) * \
+                             self.activation_derivative(sums[-1][j])
+                    deltas.append(delta)
+                    self.biases[i][j] -= learning_rate * delta
+                    for k in range(len(self.weights[i][j])):
+                        change = -learning_rate * delta * inputs[-2][k]
+                        self.weights[i][j][k] += change
+            else:
+                prev_deltas = deltas
+                deltas = []
+                for j in range(len(self.weights[i])):
+                    delta_weight_sum = 0
+                    for ll in range(len(self.weights[i + 1])):
+                        delta_weight_sum += prev_deltas[ll] * \
+                            self.weights[i + 1][ll][j]
+                    delta = delta_weight_sum * \
+                        self.activation_derivative(sums[i][j])
+                    deltas.append(delta)
+                    self.biases[i][j] -= learning_rate * delta
+                    for k in range(len(self.weights[i][j])):
+                        change = -learning_rate * delta * inputs[i][k]
+                        self.weights[i][j][k] += change
+
         return data
 
     def predict_all(
--- test_multilayer_perceptron.py
+++ test_multilayer_perceptron.py
@@ -9,7 +9,9 @@ def test_prediction_1_layer():
                   [1, 2, 3, 4]])
     ]
     biases = [np.array([-1, 0, 1])]
-    perceptron = MultilayerPerceptron(weights, biases, lambda x: x)
+    perceptron = MultilayerPerceptron(weights, biases,
+                                      (lambda x: x,
+                                       lambda x: 1))
     assert np.array_equal(
         perceptron.predict(np.array([4, 3, 2, 1])),
         np.array([8, 1, 21])
@@ -47,7 +49,9 @@ def test_prediction_more_layers():
     biases = [np.array([-2, 2]),
               np.array([-0.5, 4]),
               np.array([1])]
-    perceptron = MultilayerPerceptron(weights, biases, lambda x: max(x, 0))
+    perceptron = MultilayerPerceptron(weights, biases,
+                                      (lambda x: max(x, 0),
+                                       lambda x: int(x > 0)))
     assert np.allclose(
         perceptron.predict(np.array([1, 0, 1])),
         np.array([20])
@@ -68,3 +72,64 @@ def test_prediction_more_layers():
             np.array([14]),
         ]
     )
+
+
+def test_weight_update_final_layer():
+    weights = [
+        np.array([[1, 0, 2, 1],
+                  [0, -1, 2, 0],
+                  [1, 2, 3, 4]])
+    ]
+    biases = [np.array([-1, 0, 1])]
+    perceptron = MultilayerPerceptron(weights, biases,
+                                      (lambda x: x,
+                                       lambda x: 1))
+    perceptron.propagate(np.array([4, 3, 2, 1]), 1,
+                         np.array([1, 1, 1]))
+
+    assert np.array_equal(perceptron.weights[0],
+           np.array([[-27, -21, -12, -6],
+                     [0, -1, 2, 0],
+                     [-79, -58, -37, -16]]))
+
+    assert np.array_equal((perceptron.biases[0]),
+           np.array([-8, 0, -19]))
+
+    weights = [
+        np.array([[1., 0., 2., 1.],
+                  [0., -1., 2., 0.],
+                  [1., 2., 3., 4.]])
+    ]
+    biases = [np.array([-1., 0., 1.])]
+    perceptron = MultilayerPerceptron(weights, biases,
+                                      (lambda x: x,
+                                       lambda x: 1.))
+
+    perceptron.propagate(np.array([0.5, 1.5, -0.5, 0.25]), 1.,
+                         np.array([1., 1., 1.]))
+
+    assert np.allclose(perceptron.weights[0],
+           np.array([[2.125, 3.375, 0.875, 1.5625],
+                     [1.75, 4.25, 0.25, 0.875],
+                     [-0.5, -2.5, 4.5, 3.25]]))
+
+    assert np.allclose(perceptron.biases[0],
+           np.array([1.25, 3.5, -2.]))
+
+
+def test_weight_update_more_layers():
+    weights = [
+        np.array([[1, 2, 3],
+                  [4, 5, 6]]),
+        np.array([[-1, 1],
+                  [1, -1]]),
+        np.array([[2, 0.5]])
+    ]
+    biases = [np.array([-2, 2]),
+              np.array([-0.5, 4]),
+              np.array([1])]
+    perceptron = MultilayerPerceptron(weights, biases,
+                                      (lambda x: max(x, 0),
+                                       lambda x: int(x > 0)))
+
+    perceptron.propagate(np.array([1, 0, 1]), 1, np.array([1]))
